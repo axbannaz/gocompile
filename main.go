9625424617e19d1bccd0839ea8a331405555d6f8
Arash Bannazadeh-Mahani <axbannaz@gmail.com>
2023-07-19 21:58:59 -0400

    more complete

--- main.go
+++ main.go
@@ -3,8 +3,27 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 )
 
 func main() {
-	fmt.Printf("%v", os.Environ())
+	dotfiles := os.Getenv("dotfilesdir")
+	if len(dotfiles) == 0 {
+		df, err := os.UserHomeDir()
+		if err != nil {
+			dotfiles = "~/.files"
+		} else {
+			dotfiles = df + "/.files"
+		}
+	}
+
+	command := dotfiles + "/bin/go-install"
+	fmt.Printf("exec: %v\n", command)
+	cmd := exec.Command(command)
+	err := cmd.Wait()
+
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(255)
+	}
 }
